1a4da31a3491b2b7f3b4e002091012215e309817
PawinderGit <Pawinder@163.com>
2020-10-31 12:20:23 +0800

    添加项目文件。

---
+++ DialectsProtecting/DialectsProtecting/views.py
@@ -0,0 +1,16 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from DialectsProtecting import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        projectName='DialectsProtecting',
+    )
