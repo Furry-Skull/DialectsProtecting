aea5939e1913b550d0a7a17d8425fc58b5f1c1ef
3295363611@qq.com <3295363611@qq.com>
2020-10-31 21:10:10 +0800

    database账号密码框架

--- DialectsProtecting/DialectsProtecting/views.py
+++ DialectsProtecting/DialectsProtecting/views.py
@@ -1,15 +1,20 @@
-"""
-Routes and views for the flask application.
-"""
-
-from datetime import datetime
 from flask import render_template
 from DialectsProtecting import app
+from DialectsProtecting import Database
 
 @app.route('/')
 @app.route('/home')
 def home():
+    a=Database.Database()
+    if a.register(7,8)==0:
+        print ("cnmd")
     #主页
+    if a.login(3,9)==2:
+        print (2)
+    elif a.login(3,3)==1:
+        print (1)
+    elif a.login(3,3)==0:
+        print (0)
     return render_template(
         'index.html',
         projectName='DialectsProtecting',
