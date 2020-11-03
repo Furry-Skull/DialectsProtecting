48f0e345d045025e203b826c6a4041dba4c5f985
PawinderGit <Pawinder@163.com>
2020-11-03 15:03:28 +0800

    登录注册功能完善

--- DialectsProtecting/DialectsProtecting/views.py
+++ DialectsProtecting/DialectsProtecting/views.py
@@ -4,8 +4,8 @@ from DialectsProtecting import app, database, currentUsername, currentPassword
 
 import os
 
-#session有效期为1小时
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
+#session有效期为1天
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 #设置秘钥为随机24字节的数
 app.config['SECRET_KEY'] = os.urandom(24)
 
@@ -43,13 +43,13 @@ def loginPage():
             #返回主页
             return redirect(url_for('home'))
         elif status == 1:
-            return 'password wrong'
+            return render_template('/login.html', errorStatus = 1)
         elif status == 0:
-            return 'username not exists'
+            return render_template('/login.html', errorStatus = 0)
 
     elif request.method == 'GET':
         #访问登录页面
-        return render_template('/login.html')
+        return render_template('/login.html', errorStatus = 2)
 
 #注册界面
 @app.route('/register', methods=['GET', 'POST'])
@@ -62,7 +62,8 @@ def registerPage():
 
         status = database.register(username, password)
         if (status == 0):
-            return 'register failed'
+            #用户名已经存在
+            return render_template('/register.html', errorUsername = True)
         elif (status == 1):
             #注册后自动登录成功，保存session
             session['username'] = username
@@ -72,35 +73,4 @@ def registerPage():
 
     elif request.method == 'GET':
         #访问注册页面
-        return render_template('/register.html')
-
-#提交登录信息
-@app.route('/form/login', methods=(["POST"]))
-def loginForm():
-    form = request.form
-    username = form["用户名"]
-    password = form["密码"]
-
-    status = database.login(username, password)
-    if status == 2:
-        #登录成功
-        session['username'] = username
-        session['password'] = password
-        return 'login success'
-    elif status == 1:
-        return 'password wrong'
-    elif status == 0:
-        return 'username not exists'
-
-#提交注册信息
-@app.route('/form/register', methods=(["POST"]))
-def registerForm():
-    form = request.form
-    username = form["用户名"]
-    password = form["密码"]
-
-    status = database.register(username, password)
-    if (status == 0):
-        return 'register failed'
-    elif (status == 1):
-        return 'register successfully'
+        return render_template('/register.html', errorUsername = False)
