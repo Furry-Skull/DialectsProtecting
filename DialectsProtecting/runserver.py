9e0da1fd0164e80885be6f34594b8295490b03d8
PawinderGit <Pawinder@163.com>
2020-11-07 16:53:12 +0800

    纯净服务端提交

--- DialectsProtecting/runserver.py
+++ DialectsProtecting/runserver.py
@@ -4,4 +4,13 @@ from os import environ
 from DialectsProtecting import app
 
 if __name__ == '__main__':
-    app.run()
+    #入口函数：启动服务器，本地版本
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
+
+    #服务器版本
+    #app.run()
